a9d126077ab7c5add24950b80bd61e79a69ba212
taui11 <paulmue02@gmail.com>
2025-07-31 12:05:46 +0200

    documented export_binned plus helpers.

--- R/export_binned.R
+++ R/export_binned.R
@@ -1,15 +1,28 @@
-#' Title
+#' Export binned FASTA and BED files based on taxonomic categories
 #'
-#' @param DATA TODO
-#' @param paths_df TODO
-#' @param SAMPLE TODO
+#' This function exports contig-level sequence and annotation data into
+#' separate FASTA and BED files, grouped by taxonomic bin. It organizes the data
+#' based on a classification scheme and writes each bin’s sequences and regions
+#' to separate files for downstream analysis or visualization.
 #'
-#' @returns TODO
+#' @param DATA Data frame. Contains taxonomic and positional data used to create categories.
+#' @param paths_df Data frame. Must include columns \code{name} and \code{path} specifying locations of input and output directories/files. Typically from create_filepaths_df.
+#' @param SAMPLE Character string. Sample or dataset identifier used for naming output files.
+#'
+#' @returns NULL. Wites binned FASTA and BED files to specified directories.
 #' @export
 #'
-#' @examples TODO
+#' @examples
+#' \dontrun{
+#' paths <- data.frame(
+#'     name = c("assembly", "fasta_folder", "bed_folder"),
+#'     path = c("path/to/assembly.fasta", "path/to/fasta_bins", "path/to/bed_bins"),
+#'     stringsAsFactors = FALSE
+#' )
+#' export_binned(DATA, paths, "001")
+#' }
 export_binned <- function(DATA, paths_df, SAMPLE) {
-    fasta <- read.FASTA(paths_df$path[paths_df$name == "assembly"])
+    fasta <- ape::read.FASTA(paths_df$path[paths_df$name == "assembly"])
 
     fasta_folder <- paths_df$path[paths_df$name == "fasta_folder"]
     bed_folder <- paths_df$path[paths_df$name == "bed_folder"]
--- R/helpers_export_binned.R
+++ R/helpers_export_binned.R
@@ -1,9 +1,9 @@
-#' Title
+#' Create output directories
 #'
-#' @param fasta_folder TODO
-#' @param bed_folder TODO
+#' @param fasta_folder Character string. Path to the directory where binned FASTA files will be saved.
+#' @param bed_folder Character string. Path to the directory where binned BED files will be saved.
 #'
-#' @returns TODO
+#' @return NULL. Creates two new directories.
 #' @keywords internal
 create_output_dirs <- function(fasta_folder, bed_folder) {
     dir.create(fasta_folder, recursive = TRUE, showWarnings = FALSE)
@@ -11,11 +11,17 @@ create_output_dirs <- function(fasta_folder, bed_folder) {
 }
 
 
-#' Title
+#' Building data frame for taxonomic categories
 #'
-#' @param data TODO
+#' @param data Data frame. Contains at leas tax_cons and len column. Typically from incorporate_bam_bubbles.
 #'
-#' @returns TODO
+#' @return A data frame with columns:
+#'      \describe{
+#'          \item{chrom}{Chromosome/contig names (rownames of input).}
+#'          \item{chromStart}{Start position, always 0.}
+#'          \item{chromEnd}{End position, taken from the `len` column of input data.}
+#'          \item{bin}{Factor vector representing bins labeled by taxonomic consensus.}
+#'      }
 #' @keywords internal
 build_categories_df <- function(data) {
     data$bin_label <- paste0("bin_", data$tax_cons)
@@ -27,35 +33,35 @@ build_categories_df <- function(data) {
     )
 }
 
-#' Title
+#' Export FASTA sequences grouped by taxonomic bins
 #'
-#' @param fasta TODO
-#' @param categories TODO
-#' @param fasta_folder TODO
-#' @param sample_name TODO
+#' @param fasta List of class "DNAbin" or "AAbin".
+#' @param categories Data frame. Contains at least a bin column. Typically from build_categories_df.
+#' @param fasta_folder Character string. Path to the binned FASTA directory.
+#' @param sample_name Character string. Sample/Title identifier of the data (e.g. "001").
 #'
-#' @returns TODO
+#' @return NULL. Saves binned FASTA files in the fasta_folder directory.
 #' @keywords internal
 export_fasta_bins <- function(fasta, categories, fasta_folder, sample_name) {
     for (bin_level in levels(categories$bin)) {
         contigs <- categories$chrom[categories$bin == bin_level]
         out_fasta <- file.path(fasta_folder, paste0(sample_name, "_", bin_level, ".fa"))
-        write.FASTA(fasta[contigs], out_fasta)
+        ape::write.FASTA(fasta[contigs], out_fasta)
     }
 }
 
-#' Title
+#' EXPORT BED files grouped by taxonomic bins
 #'
-#' @param categories TODO
-#' @param bed_folder TODO
-#' @param sample_name TODO
+#' @param categories Data frame. Contains at least a bin column. Typically from build_categories_df.
+#' @param bed_folder Character string. Path to the binned BED directory.
+#' @param sample_name Character string. Sample/Title identifier of the data (e.g. "001").
 #'
-#' @returns TODO
+#' @return NULL. Saves binned BED files in the bed_folder directory.
 #' @keywords internal
 export_bed_bins <- function(categories, bed_folder, sample_name) {
     for (bin_level in levels(categories$bin)) {
         bed_data <- categories[categories$bin == bin_level, 1:3]
         out_bed <- file.path(bed_folder, paste0(sample_name, "_", bin_level, ".bed"))
-        write.table(bed_data, out_bed, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+        utils::write.table(bed_data, out_bed, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
     }
 }
